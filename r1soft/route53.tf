a605dc293a7c282b8d1741f4c4051a2bd015d0d8
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-08-07 20:27:02 -0500

    test

--- r1soft/route53.tf
+++ r1soft/route53.tf
@@ -3,5 +3,5 @@ resource "aws_route53_record" "r1soft" {
   name    = "${var.record}.${var.domain}"
   type    = "A"
   ttl     = "300"
-  records = ["${aws_instance.jenkins.public_ip}"]
+  records = ["${aws_instance.r1soft.public_ip}"]
 }
