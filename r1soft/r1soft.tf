eca716b7d7c822278ca052d5da0991794687b6f3
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-08-07 20:26:10 -0500

    test

--- r1soft/r1soft.tf
+++ r1soft/r1soft.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "r1soft"{
     ami = "${var.ami}"
     instance_type = "${var.instance_type}"
-    key_name = "${aws_key_pair.developer.id}"
+    key_name = "${aws_key_pair.developer_key.id}"
     associate_public_ip_address = true
     security_groups = ["${aws_security_group.jenkins.name}"]
 
--- r1soft/sec_group.tf
+++ r1soft/sec_group.tf
@@ -14,8 +14,8 @@ resource "aws_security_group" "r1soft" {
   }
   ingress {
     # TLS (change to whatever ports you need)
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 8443
+    to_port     = 8443
     protocol    = "tcp"
     # Please restrict your ingress to only necessary IPs and ports.
     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
