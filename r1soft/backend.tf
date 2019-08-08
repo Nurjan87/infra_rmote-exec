ee9c63d7c62ce0b9685329e05f00ff9405385103
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-08-08 10:16:21 -0500

    aded s3

--- r1soft/backend.tf
+++ r1soft/backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         region = "us-west-1"
-        bucket = "remote-exec-nurjan"
+        bucket = "r1soft-terraform-nurjan"
         key = "r1soft/r1soft.state"
     }
 }
